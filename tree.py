53ac5e8019420a1d4aa5c2d5e3e8d58bbbbfb39b
Metamystical <metamystical@users.noreply.github.com>
2024-08-03 04:07:01 +0200

    Add files via upload

--- tree.py
+++ tree.py
@@ -1,173 +1,56 @@
 import pyautogui as pg
 import pyperclip as pc
-from pynput.mouse import Controller; mouse = Controller()
 import re
 import time
-import sqlite3
-import os
 import sys
-
-contract = (21, 62)
-center = ('', 681, 550)
-    
-sectors = [
-    ('F', 624, 510),
-    ('FF', 574, 539),
-    ('FFF', 508, 562),
-    ('FFFF', 422, 599),
-    ('FFFM', 419, 541),
-    ('FFM', 521, 486),
-    ('FFMF', 429, 479),
-    ('FFMM', 454, 421),
-    ('FM', 630, 455),
-    ('FMF', 568, 422),
-    ('FMFF', 488, 373),
-    ('FMFM', 539, 334),
-    ('FMM', 642, 385),
-    ('FMMF', 589, 306),
-    ('FMMM', 649, 296),
-    ('M', 735, 511),
-    ('MF', 730, 455),
-    ('MFF', 721, 385),
-    ('MFFF', 710, 303),
-    ('MFFM', 772, 312),
-    ('MFM', 793, 421),
-    ('MFMF', 830, 335),
-    ('MFMM', 871, 377),
-    ('MM', 789, 533),
-    ('MMF', 841, 484),
-    ('MMFF', 912, 422),
-    ('MMFM', 932, 475),
-    ('MMM', 851, 563),
-    ('MMMF', 942, 538),
-    ('MMMM', 938, 599),  
-]
-
-expand = [
-    ('FFFF', 464, 596),
-    ('FFFM', 459, 544),
-    ('FFMF', 468, 492),
-    ('FFMM', 488, 444),
-    ('FMFF', 519, 401),
-    ('FMFM', 558, 366),
-    ('FMMF', 606, 344),
-    ('FMMM', 657, 331),
-    ('MFFF', 709, 330),
-    ('MFFM', 761, 343),
-    ('MFMF', 807, 369),
-    ('MFMM', 845, 399),
-    ('MMFF', 879, 444),
-    ('MMFM', 899, 494),
-    ('MMMF', 907, 545),
-    ('MMMM', 903, 598)
-]
-
-pattern = re.compile('^[A-Z1-9]{4}-[A-Z1-9]{3}$') # for code
-
-found = False
-for window in pg.getAllWindows():
-    if re.search('^Home • FamilySearch.*', window.title):
-        found = True
-        window.activate()
-        time.sleep(1)
-        break
-if not found: sys.exit()
-
-db = 'tree.db'
-is_db = False
-if os.path.isfile(db): is_db = True
-connection = sqlite3.connect('tree.db')
-cursor = connection.cursor()
-if is_db: print('Existing db opened')
-else:
-    cursor.execute('create table tree (id integer primary key autoincrement, chain text not null unique, code varchar(8) not null, name text, birth_date text, birth_place text, death_date text, death_place text)')
-    print ('New db created')
-#sys.exit()
+import utils
 
 def crash(mess):
-    print(mess); sys.exit()
-    
-def insert(tup):
-    def insertcc(cc):
-        try: cursor.execute('insert into chains (chain, code) values (?, ?)', cc)
-        except sqlite3.IntegrityError as e: print(f'Chain already exists: {cc[0]}')
-        
-    chain = tup[0:1][0]
-    code = tup[1:2][0]
-    result = cursor.execute('select code from tree where code = ?', (code,)).fetchone()
-    if result == None:
-        #print('ancestor does not exist, adding', code, chain)
-        cursor.execute('insert into tree (code, name, birth_date, birth_place, death_date, death_place) values (?, ?, ?, ?, ?, ?)', tup[1:])
-        insertcc(tup[:2])
-        connection.commit()
-        return([])
-    # ancestor already existed; add every ancestor further up the tree
-    #print('ancestor existed', code, chain)
-    bases = cursor.execute("select chain from chains where code = ?", (code,)).fetchall()
-    ccss = []
-    for bs in bases:
-        ccs = cursor.execute("select chain, code from chains where chain like ?", (f'{bs[0]}%',)).fetchall()
-        ccs = [(cs[0][len(bs[0]):], cs[1]) for cs in ccs] # strip base
-        ccss += ccs
-    ccss = list(set(ccss)) # removes duplicates
-    ret = []
-    for ccs in ccss:
-        if ccs[1] == code: continue
-        ch = chain + ccs[0]
-        # print('adding:', *ccs, ch)
-        insertcc((ch, ccs[1]))
-        ret.append(ch)
-    connection.commit()
-    return(ret)
-
-def get_code(chain): # returns code string or empty string if no code found
-    code = cursor.execute("select code from chains where chain = '" + chain + "'").fetchone()
-    if code == None: return ''
-    return code[0]
-
-def get_counts():
-    def get_count(table):
-        return(cursor.execute(f'select count(*) from {table}').fetchone()[0])
-    print('#ancestors:', get_count('tree'), get_count('chains'))
+    print(mess); utils.close_db(); sys.exit()
 
 def grab(x, y, nofollow): # get the details of an ancestor at x,y; return tuple, could be ()
     pc.copy('xyzzy')
-    pg.click(x, y); time.sleep(0.4) # open detail popup
+    pg.click(x, y); time.sleep(0.5) # open detail popup
     pg.hotkey('ctrl', 'a'); pg.hotkey('ctrl', 'c') # select all, copy
-    if(pc.paste() == 'xyzzy'): return(())
-    lines = pc.paste().splitlines()
+    line = pc.paste()
+    for round in range(4): # three tries
+        if round == 3: return(())
+        if line == 'xyzzy':
+            pg.press('esc'); time.sleep(0.5) # close detail popup
+            pg.click(x, y); time.sleep(0.5); pg.hotkey('ctrl', 'a'); pg.hotkey('ctrl', 'c')
+            line = pc.paste()
+            continue
+        break
     offset = 2
-    def refresh():
-        time.sleep(0.4)
-        pg.hotkey('ctrl', 'a'); pg.hotkey('ctrl', 'c')
-        lines = pc.paste().splitlines()
     def get_line(index):
+        lines = pc.paste().splitlines()
         for round in range(4): # three tries
             if round == 3: crash('line not found')
             try: line = lines[index]
             except Exception as e:
-                refresh()
+                time.sleep(0.5); pg.hotkey('ctrl', 'a'); pg.hotkey('ctrl', 'c')
+                lines = pc.paste().splitlines()
                 continue
             break
         return(line)
     code = ''
+    pattern = re.compile('^[A-Z1-9]{4}-[A-Z1-9]{3}$') # for code
     for round in range(4): # three tries
         if round == 3: return(())
         code1 = get_line(offset + 24)[:-2]
         m1 = bool(pattern.match(code1))
         code2 = get_line(offset + 25)[:-2]
         m2 = bool(pattern.match(code2))
-        if not m1 and not m2:
-            refresh()
-            continue
+        if not m1 and not m2: continue
         if m2: # profile has a picture
              offset += 1
              code = code2
         else: code = code1
         break
+    pg.press('esc'); time.sleep(0.5) # close detail popup
     name = get_line(offset + 23)
+    if name.capitalize() == 'Unknown': return(())
     if nofollow: offset -= 1
-    pg.press('esc'); time.sleep(0.4) # close detail popup
     f = []
     f.append(get_line(offset + 26))
     f.append(get_line(offset + 27))
@@ -206,11 +89,52 @@ def grab(x, y, nofollow): # get the details of an ancestor at x,y; return tuple,
     death_date, death_place = get_pair(death, sources)
     return((code, name, birth_date, birth_place, death_date, death_place))
 
+def grab_center(code): # crash upon fail
+    for round in range(4): # three tries
+        if round == 3: crash('fan not loaded')
+        offset = -4 + 8 * round # in case of no last name
+        ch, x, y = center # go to center
+        tup = grab(x, y + offset, code == '')
+        if len(tup) == 0:
+            time.sleep(0.5)
+            continue
+        if (code == '' or code == tup[0]): return(tup)
+        crash(f'code mismatch {code} vs {tup[0]} at {tup[1]}')
+        return(tup)
+
 def gray(x, y):
     color = pg.pixel(x, y) # returns RGB triplet
     if color == (242, 242, 242) or color == (232, 232, 232) or color == (216, 216, 216): return(True)
     return(False)
-    
+
+def insert(tup):
+    chain = tup[0:1][0]
+    code = tup[1:2][0]
+    if not utils.is_code(code):
+        utils.insert_tree(tup[1:])
+        utils.insert_chains(tup[:2])
+        utils.commit()
+        return([])
+    # ancestor already existed; add every ancestor further up the tree
+    #print('ancestor existed', code, chain)
+    bases = utils.get_chains(code)
+    #print('bases', bases)
+    ccss = []
+    for bs in bases:
+        ccs = utils.get_ccs_with_base(bs)
+        ccs = [(cs[0][len(bs):], cs[1]) for cs in ccs] # strip base
+        ccss += ccs
+    ccss = list(set(ccss)) # removes duplicates
+    #print('ccss', ccss)
+    ret = []
+    for ccs in ccss:
+        ch = chain + ccs[0]
+        #print('adding:', *ccs, ch)
+        utils.insert_chains((ch, ccs[1]))
+        ret.append(ch)
+    utils.commit()
+    return(ret)
+
 def fan(origin): # insert the details of every ancestor in the fan into the db
     skip = []
     for ch, x, y in sectors:
@@ -219,63 +143,39 @@ def fan(origin): # insert the details of every ancestor in the fan into the db
         color = pg.pixel(x, y) # returns RGB triplet
         if gray(x, y): continue
         tup = grab(x, y, False)
-        if len(tup) > 0: skip = insert((chain,) + tup)
-    get_counts()
+        if len(tup) > 0: skip += insert((chain,) + tup)
+    print('#ancestors:', utils.get_count('tree'), utils.get_count('chains'))
 
-def grab_center(code): # crash upon fail
-    for round in range(4): # three tries
-        if round == 3: crash('fan not loaded')
-        time.sleep(1)
-        offset = -10 + 10 * round # in case of no last name
-        ch, x, y = center # go to center
-        tup = grab(x, y + offset, code == '')
-        if len(tup) == 0: continue
-        if (code == '' or code == tup[0]): return(tup)
-        print(f'code mismatch {code} vs {tup[0]} at {tup[1]}')
-        return(tup)
+#title = 'Home • FamilySearch'
+title = 'Dr. John'
+utils.activate('^' + title + '.*')
+sectors = utils.fan()
+center = sectors.pop(0)
+is_db = utils.open_db('tree.db')
 
-if not is_db: # grab and insert home
+if not is_db: # grab and insert home fan
+    utils.setup()
     tup = grab_center('')
     insert(('',) + tup)
     fan('')
 
-start = 80
-step = 10
-with open('level5.csv', 'r') as file: lines = file.readlines()
-print("Leve5", len(lines))
-level5 = [line.rstrip('\n') for line in lines[start:start + step]]
-print(f"Level5: {start} to {start + step - 1} inclusive")
-for i in range(0, step):
-    base = level5[i]
-    if len(base) % 4 != 0 or not bool(re.match(r'[FM]*$', base)): crash('bad base')
-    # load base fan
-    base_code = get_code(base)
+generation = 6 # multiples of 6, not including me
+gen = utils.get_gen(generation)
+print(f"Generation {generation}: {len(gen)} ancestors")
+start = 0 # start at 0
+step = 42 # add this to start when ready for next run
+print(f"Gen {generation}: {start} to {start + step - 1} inclusive")
+for i in range(start, start + step):
+    base = gen[i]
+    if len(base) % 6 != 0 or not bool(re.match(r'[FM]*$', base)): crash('bad base')
+    base_code = utils.get_code(base)
     print(i, base, base_code)
     if len(base_code) != 8: crash('db error')
-    pg.click(206, 62); time.sleep(0.2)
+    pg.click(206, 62); time.sleep(0.5)
     pc.copy('https://www.familysearch.org/tree/pedigree/fanchart/' + base_code)
     pg.hotkey('ctrl', 'v'); time.sleep(0.2) # paste
-    pg.press('enter'); time.sleep(2)
+    pg.press('enter'); time.sleep(5)
+    utils.setup()
     grab_center(base_code) # confirm
-    fan(base) # grab base fan
-    for target in expand:
-        # load target fan one level up
-        new_base = base + target[0]
-        new_code = get_code(new_base)
-        #print('new', new_code, new_base)
-        if len(new_code) != 8: continue
-        ch, x, y = [tup for tup in expand if tup[0] == new_base[-4:]][0]
-        # test for missing sector
-        if gray(x, y):
-            print(f'{new_code} is no longer an ancestor at {new_base}')
-            continue
-        pg.click(x, y); time.sleep(2) # load new fan
-        grab_center(new_code)
-        fan(new_base)
-        x, y = contract # return to base via back arrow
-        pg.click(x, y)
-        #print('mark', new_base, new_code, base_code)
-        grab_center(base_code) 
-
-cursor.close()
-connection.close()
+    fan(base)
+utils.close_db()
---
+++ utils.py
@@ -0,0 +1,134 @@
+import pyautogui as pg
+import math
+import re
+import time
+import sys
+import os
+import sqlite3
+
+def open_db(db):
+    global connection, cursor
+    is_db = False
+    if os.path.isfile(db): is_db = True
+    connection = sqlite3.connect('tree.db')
+    cursor = connection.cursor()
+    if is_db: print('existing db opened')
+    else:
+        cursor.execute('create table tree (id integer primary key autoincrement, code varchar(8) not null, name text, birth_date text, birth_place text, death_date text, death_place text)')
+        cursor.execute('create table chains (chain text not null unique, code varchar(8) not null)')
+        print('new db created')
+    return(is_db)
+
+def exec_ret(command, tup): # returns [] if no result, otherwise list of tuples
+    return(cursor.execute(command, tup).fetchall())
+
+def exec(command, tup):
+    try: cursor.execute(command, tup)
+    except Exception as e:
+        #print(e)
+        return(False)
+    return(True)
+
+def is_code(code):
+    return(len(exec_ret('select code from tree where code = ?', (code,))) >= 1)
+
+def insert_tree(tup):
+    exec('insert into tree (code, name, birth_date, birth_place, death_date, death_place) values (?, ?, ?, ?, ?, ?)', tup)
+
+def insert_chains(tup):
+    exec('insert into chains (chain, code) values (?, ?)', tup)
+
+def detuple_list(tups):
+    if tups == None: return([])
+    return([tup[0] for tup in tups])
+
+def get_code(chain): # returns code string or empty string if no code found
+    res = exec_ret('select code from chains where chain = ?', (chain,))
+    res = detuple_list(res)
+    if len(res) == 0: return ''
+    return res[0]
+
+def get_chains(code): # returns list of chain strings
+    res = exec_ret("select chain from chains where code = ?", (code,))
+    return(detuple_list(res))
+
+def get_gen(generation): # returns list of chain strings
+    res = exec_ret('select distinct chain from chains where length(chain) = ? order by chain', (generation,))
+    return(detuple_list(res))
+
+def get_ccs_with_base(base): # returns list of tuples
+    res = exec_ret("select chain, code from chains where chain like ?", (f'{base}%',))
+    if res == None: return([])
+    return(res)
+
+def get_count(table):
+    return(exec_ret(f'select count(*) from {table}', ())[0][0])
+
+def commit():
+    connection.commit()
+
+def close_db():
+    cursor.close()
+    connection.close()
+    print('db closed')
+
+def activate(title):
+    found = False
+    for window in pg.getAllWindows():
+        if re.search('^' + title + '.*', window.title):
+            found = True
+            window.activate()
+            time.sleep(2)
+            break
+    if not found:
+        print('window not found:', title)
+        sys.exit()
+
+def setup():
+    pg.click(1275, 303); time.sleep(1) # full screen
+    pg.click(1247, 137); time.sleep(1) # center
+
+# fan should be set to 7 generations and sized to maximum while still having all sectors clickable
+# enter x,y screen coordinates of center (xc, yc), and lower left sectors going from ring 1 to ring ringmax - 1
+# and separately ring ringmax - 1 lower right (xr, yr) below for calibration
+def fan():
+    coords = [(684, 535), (641, 502), (596, 526), (546, 545), (474, 576), (406, 608), (330, 640)]
+    xc, yc = coords[0]
+    xr, yr = (1034, 640) 
+    ringmax = 7
+
+    def radius_and_angle(tup):
+        x, y = tup
+        radius = math.sqrt((x - xc)**2 + (y - yc)**2)
+        angle_rad = math.atan2(y - yc, x - xc)
+        return radius, angle_rad
+
+    rs = [0]
+    for i in range(1, ringmax): rs.append(radius_and_angle(coords[i])[0])
+    a_ll = radius_and_angle(coords[ringmax - 1])[1] # lower left outer ring sector angle
+    da = (2 * math.pi - a_ll + radius_and_angle((xr, yr))[1]) / (2**(ringmax - 1) - 1)
+    rings = []
+    for ri in range(0, ringmax):
+        ring = []
+        factor = 2 ** (ringmax - 1 - ri)
+        sa = a_ll + da * (factor - 1) / 2 
+        for i in range(2**ri):
+            a = sa + da * i * factor
+            ring.append((int(xc + rs[ri] * math.cos(a)), int(yc + rs[ri] * math.sin(a))))
+        rings.append(ring)
+    sectors = ['']
+    def permus(chain, length):
+        if length == 0:
+            sectors.append(chain)
+            return
+        permus(chain + 'F', length - 1)
+        permus(chain + 'M', length - 1)
+    for length in range(1, len(rings)): permus('', length)
+    sec = 0
+    for ri in range(0, len(rings)):
+        for i in range(len(rings[ri])):
+            sectors[sec] = (sectors[sec], ) + rings[ri][i]
+            sec += 1
+    return(sectors)
+##        pg.moveTo(rings[ri][i][0], rings[ri][i][1])
+##        time.sleep(1)
